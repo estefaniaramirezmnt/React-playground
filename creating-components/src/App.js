16aa3732f3346637827a45c540db690108ca1e69
Estefania <estefaniaramirezmnt@gmail.com>
2023-05-30 14:45:09 +0200

    Created creating-component React app

---
+++ creating-components/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header() {
+  return <h1>Hello World</h1>;
+  //You can also can write it like this: return (<h1>Hello world</h1>), i.e. between parenthesis
+}
+
+function App() {  
+  return <Header/>;
+}
+
+export default App;
